f25f4455e03cf33206806a186b8e860d1e4a0daf
invsblmad <aizatmadumarova04@gmail.com>
2023-05-01 12:03:46 +0600

    renamed exception class

--- src/main/java/com/inai/arna/exception/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.inai.arna.exception;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
